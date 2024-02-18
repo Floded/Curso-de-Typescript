44347bea9d37ab08d08060527ed3bf474e38314e
Alfredo Lillo <alphonselillo@gmail.com>
2024-02-18 18:06:19 -0300

    typescript finalizado

--- main.js
+++ main.js
@@ -1,48 +1,12 @@
 "use strict";
-(() => {
-    let ironMan = {
-        name: "Tony Stark",
-        age: 54,
-        power: ["philantropo", "Rich"],
+var Validations;
+(function (Validations) {
+    Validations.validateText = (test) => {
+        return test.length > 3 ? true : false;
     };
-    let drStrage = {
-        name: "Steve",
-        age: 80,
-        power: ["Levitacion"],
+    Validations.validateDate = (myDate) => {
+        return isNaN(myDate.valueOf()) ? false : true;
     };
-})();
-(() => {
-})();
-(() => {
-    const client = {
-        name: "Luis",
-        age: 36,
-        address: {
-            id: 52,
-            codPostal: "KY2 U45",
-            city: "San Juan",
-        },
-        getFullAdress(id) {
-            return this.address.city;
-        },
-    };
-    const client2 = {
-        name: "Marisa",
-        age: 34,
-        address: {
-            id: 524,
-            codPostal: "SJ5 KK3",
-            city: "San Juan",
-        },
-        getFullAdress(id) {
-            return this.address.city;
-        },
-    };
-})();
-(() => {
-    let addNumbersFunctions;
-    addNumbersFunctions = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText("Lui"));
 //# sourceMappingURL=main.js.map
